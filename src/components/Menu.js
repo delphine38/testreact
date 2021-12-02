a2848ea807e08750914a52a1b7224e7ae811ef27
delphine <delphin38@live.fr>
2021-12-02 11:29:12 +0100

    cdn bootstrap and link stop

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,19 +4,23 @@ import {Link, NavLink} from 'react-router-dom';
 function Menu(){
 return(
     <div>
-        <a href="/">docs</a>
-                <br></br>
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" a href="/Menu">Menu</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" a href="/Docs">Docs</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" a href="/Community">Community</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" a href="/Tuto">Tuto</a>
+  </li>
+</ul>
 
 
-        <a href="/Community">community</a>
-        <br></br>
-
-        
-
-        <a href="/Menu">menu</a>
-                <br></br>
-
-        <a href="/Tuto">tuto</a>
+<h1 className="text-center">Bonjour et bienvenue sur mon premier essaie de site react test avec Bootstrap</h1>
 
     </div>
 )
